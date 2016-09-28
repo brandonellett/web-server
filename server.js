7db16122646b4634c3758d684b0cac9b6ad19a07
Brandon Ellett <brandonellett@gmail.com>
2016-09-28 11:06:24 -0600

    Initialize repo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express =  require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+    requireAuth: function(req, res, next){
+        console.log('private route hit!');
+        next();
+    },
+    logger: function(req, res, next){
+        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuth, function(req, res){
+    res.send('About Us');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+    console.log('Express server listening on port: ' + port);
+});
