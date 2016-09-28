05cf942cdd71f59f9c9a78a790dd939fda888df8
Brandon Ellett <brandonellett@gmail.com>
2016-09-28 13:38:11 -0600

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
 var express =  require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'Fart',
+    completed: false
+}, {
+    id: 2,
+    description: 'Kiss wife',
+    completed: false
+},{
+    id: 3,
+    description: 'Make sweet app',
+    completed: true
+}];
 
-var middleware = require('./middleware');
+//var middleware = require('./middleware');
+//
+//app.use(middleware.logger);
+//
+//app.get('/about', middleware.requireAuth, function(req, res){
+//    res.send('About Us!');
+//})
+app.get('/', function(req, res){
+    res.send('Todo API Root');
+});
 
-app.use(middleware.logger);
+app.get('/todos', function(req, res){
+    res.json(todos); 
+});
 
-app.get('/about', middleware.requireAuth, function(req, res){
-    res.send('About Us!');
-})
+app.get('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+    
+    todos.forEach(function(todo){
+        if(todoId === todo.id){
+            matchedTodo = todo;
+        }    
+    });
+    
+    if(matchedTodo){
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
+});
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function(){
     console.log('Express server listening on port: ' + PORT);
