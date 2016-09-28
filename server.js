75fb1a5fa29c2048445e8b68baabffd3ff93697e
Brandon Ellett <brandonellett@gmail.com>
2016-09-28 14:54:20 -0600

    Add Post for todos

--- server.js
+++ server.js
@@ -1,35 +1,23 @@
 var express =  require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Fart',
-    completed: false
-}, {
-    id: 2,
-    description: 'Kiss wife',
-    completed: false
-},{
-    id: 3,
-    description: 'Make sweet app',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
 
-//var middleware = require('./middleware');
-//
-//app.use(middleware.logger);
-//
-//app.get('/about', middleware.requireAuth, function(req, res){
-//    res.send('About Us!');
-//})
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
     res.send('Todo API Root');
 });
 
+//GET /todos
 app.get('/todos', function(req, res){
     res.json(todos); 
 });
 
+//GET /todos/:id
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo;
@@ -47,7 +35,16 @@ app.get('/todos/:id', function(req, res){
     }
 });
 
-//app.use(express.static(__dirname + '/public'));
+//POST /todos
+app.post('/todos', function(req, res){
+    var body = req.body;
+    
+    body.id = todoNextId++;
+    
+    todos.push(body);
+    
+    res.json(body);
+});
 
 app.listen(PORT, function(){
     console.log('Express server listening on port: ' + PORT);
