42c1bf5d4309f959c9feaca0a8c867dafcc9b74e
Brandon Ellett <brandonellett@gmail.com>
2016-09-28 12:27:48 -0600

    Update port for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express =  require('express');
 var app = express();
-var port = 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware');
 
@@ -12,6 +12,6 @@ app.get('/about', middleware.requireAuth, function(req, res){
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function(){
-    console.log('Express server listening on port: ' + port);
+app.listen(PORT, function(){
+    console.log('Express server listening on port: ' + PORT);
 });
